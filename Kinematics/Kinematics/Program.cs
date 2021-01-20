8ff31a988b3b77b4df039397d93128186b61986e
Advait Patel <beinspired118@gmail.com>
2021-01-20 13:23:52 -0600

    initial commit

---
+++ Kinematics/Kinematics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kinematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //v = dx/dt  --->  dx = v*dt
+            //dt = 0.1
+
+
+            //initialize velocity variable to 5 m/s
+            //using decimal instead of double to get rid of annoying floating point errors
+            decimal velocity = 5;
+            //initialize position variable
+            decimal x = 0;
+            Console.WriteLine("Time (s)" + "\tPosition (m)" + "\tVelocity (m/s)");
+            for (decimal time = 0; time <= 100; time += (decimal)0.1) 
+            {
+
+                Console.WriteLine(time + "\t" + x + "\t" + velocity);
+
+                x = x + velocity * (decimal)0.1;
+
+            }
+            Console.WriteLine("Done");
+
+
+        }
+    }
+}

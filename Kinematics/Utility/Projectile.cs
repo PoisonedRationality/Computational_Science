c8e6c232e5fd65a63d944c524ac35925ce74b362
Advait Patel <beinspired118@gmail.com>
2021-02-05 01:41:42 -0600

    finished hw 2.3 classes

---
+++ Kinematics/Utility/Projectile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility
+{
+    class Projectile
+    {
+
+        public Projectile(Vector pos, Vector velo, Vector accel, double mass)
+        {
+            Position = pos;
+
+            Velocity = velo;
+
+            Acceleration = accel;
+
+            Mass = mass;
+        }
+
+        public Projectile(double mass)
+        {
+            Position = Velocity = Acceleration = new Vector(0, 0, 0);
+            Mass = mass;
+        }
+
+        public Vector Position { get; set; }
+
+        public Vector Velocity { get; set; }
+
+        public Vector Acceleration { get; set; }
+
+        public double Mass { get; }
+
+        public void Move(double dt) 
+        {
+            UpdateVelocity(dt);
+            UpdatePosition(dt);
+        }
+
+        public void ApplyForce(Vector force) 
+        {
+            Acceleration = force / Mass;
+        }
+
+        private void UpdatePosition(double dt)
+        {
+            Position += Velocity * dt;
+        }
+
+        private void UpdateVelocity(double dt)
+        {
+            Velocity += Acceleration * dt;
+        }
+    }
+}
---
+++ Kinematics/Utility/World.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility
+{
+    class World
+    {
+
+        public World(double time, double c, double g_accel, List<Projectile> projectiles)
+        {
+            Time = time;
+            C = c;
+            G_Accel = g_accel;
+
+            Projectiles = projectiles;
+        }
+
+        public double Time { get; set; }
+
+        public double C { get; set; }
+
+        public double G_Accel { get; set; }
+
+        public List<Projectile> Projectiles { get; set; }
+
+        public void ApplyForcesToProjectiles()
+        {
+            foreach (Projectile proj in Projectiles)
+            {
+                var f_gravity = new Vector(0, 0, G_Accel);
+                var f_air = -C * proj.Velocity.Magnitude * proj.Velocity.Magnitude * Vector.Normalize(proj.Velocity);
+                var f_net = f_gravity + f_air;
+
+                proj.ApplyForce(f_net);
+            }
+            
+        }
+
+        public void Tick(double dt)
+        {
+            Time += dt;
+            ApplyForcesToProjectiles();
+            foreach (Projectile proj in Projectiles)
+            {
+                proj.Move(dt);
+            }
+        }
+    }
+}
